a4de49480131ca00d8de4bbbf99581f790642c8c
aminoa <28660350+aminoa@users.noreply.github.com>
2023-03-26 00:21:58 -0400

    word set and train script

---
+++ traincheck.py
@@ -0,0 +1,4 @@
+# TODO: have dataset of train stops and the associated train stop connections with the trains that connect to it
+# TODO: import that data into a graph
+# TODO: import the valid words into a trie
+# TODO: find the longest word in the trie that is also a valid path in the graph
