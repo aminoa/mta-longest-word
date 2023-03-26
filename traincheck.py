c33653f078fd2968a91fd3a2270b15027247cefc
aminoa <28660350+aminoa@users.noreply.github.com>
2023-03-26 10:11:52 -0400

    more work

--- traincheck.py
+++ traincheck.py
@@ -1,4 +1,36 @@
-# TODO: have dataset of train stops and the associated train stop connections with the trains that connect to it
-# TODO: import that data into a graph
-# TODO: import the valid words into a trie
+import csv
+import networkx as nx
+import matplotlib.pyplot as plt
+from pygtrie import StringTrie
+
+mta = nx.Graph()
+
+with open('train_stops.csv', 'r') as f:
+    next(f)
+    reader = csv.reader(f)
+    for train_start, train_end, line in reader:
+        train_start = train_start[:3]
+        train_end = train_end[:3]
+
+        if train_start not in mta:
+            mta.add_node(train_start)
+        if train_end not in mta:
+            mta.add_node(train_end)
+
+        mta.add_edge(train_start, train_end, line=line)
+
+# nx.draw(mta, with_labels=True)
+# plt.show()
+# plt.savefig("filename.png")
+
+t_words = StringTrie()
+with open('valid_words.txt', 'r') as f:
+    file_words = f.read().splitlines()
+    for word in file_words:
+        # print(word)
+        if word:
+            t_words[str(word)] = True
+
+
 # TODO: find the longest word in the trie that is also a valid path in the graph
+
